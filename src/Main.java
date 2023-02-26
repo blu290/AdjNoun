5460f5062d0be9713ca8b85e9e736f215d5d46a2
test <darudeshrekstorm@gmail.com>
2023-02-26 13:42:24 +0000

    gui version

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Random;
+
+
+
 public class Main {
     public static ArrayList<String> readFile(String fileName){
         ArrayList<String> wordList = new ArrayList<>();
@@ -19,23 +21,15 @@ public class Main {
         }
         return wordList;
     }
+
+
     public static void main(String[] args) {
         //System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        boolean filesExist = true;
         ArrayList<String> adjectiveList = readFile("a.txt");
         ArrayList<String> nounList = readFile("n.txt");
-        Random randomCalc = new Random();
-        Scanner input = new Scanner(System.in);
-        System.out.println("Press 'q' to quit, any other button to generate a new funny");
-        if(adjectiveList.size() == 0||nounList.size() == 0)
-            filesExist = false;
-        while(filesExist){
-            if (input.nextLine().equals("q")){
-                filesExist = false;
-                break;
-            }
-            System.out.println(adjectiveList.get(randomCalc.nextInt(adjectiveList.size())) + " " +nounList.get(randomCalc.nextInt(nounList.size())) );
-
-        }
+        if (!(nounList.size() == 0 || adjectiveList.size() == 0))
+            new GUI(adjectiveList,nounList);
+        else
+            System.out.println("you're missing the files!");
     }
 }
